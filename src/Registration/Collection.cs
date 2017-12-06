d08b2dab8e93428f388ce9d9d6d825de96926f3c
Eugene Sadovoi <evgeni@eniks.com>
2017-12-06 00:02:39 -0500

    Added Registrations collection

---
+++ src/Registration/Collection.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unity.Specification.TestData;
+
+namespace Unity.Specification.Registration
+{
+    public abstract partial class SpecificationTests : TestFixtureBase
+    {
+        [TestMethod]
+        public void IUnitContainer_Registrations()
+        {
+            _container.RegisterType<IService, Service>();
+            _container.RegisterType<IService, Service>("first");
+            _container.RegisterType<IService, Service>("second");
+
+            var reg0 = _container.Registrations.ToArray();
+
+            using (IUnityContainer child = _container.CreateChildContainer())
+            {
+                child.RegisterType<IService, Service>("third");
+
+                var reg1 = child.Registrations.ToArray();
+            }
+
+            _container.RegisterType<IService, Service>("fourth");
+
+            var reg2 = _container.Registrations.ToArray();
+        }
+
+    }
+}
