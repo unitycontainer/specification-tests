a00d5a84f7f8c7a97faa9b3d7a677c8b953416a7
Eugene Sadovoi <evgeni@eniks.com>
2018-03-01 23:28:02 -0500

    #67

--- src/Issues/GitHubIssues.cs
+++ src/Issues/GitHubIssues.cs
@@ -12,6 +12,31 @@ namespace Unity.Specification.Issues
     public abstract partial class ReportedIssuesTests
     {
         [TestMethod]
+        public void unitycontainer_container_67()
+        {
+            var container = GetContainer();
+
+            container.RegisterType<ILogger, MockLogger>(new TransientLifetimeManager());
+
+            var child = container.CreateChildContainer();
+
+            child.RegisterType<OtherService>(new TransientLifetimeManager());
+
+            Assert.IsTrue(child.IsRegistered<ILogger>());
+            Assert.IsFalse(child.IsRegistered<MockLogger>());
+            Assert.IsTrue(child.IsRegistered<OtherService>());
+
+            container.RegisterType<IOtherService, OtherService>();
+
+            child = child.CreateChildContainer();
+
+            Assert.IsTrue(child.IsRegistered<ILogger>());
+            Assert.IsFalse(child.IsRegistered<MockLogger>());
+            Assert.IsTrue(child.IsRegistered<IOtherService>());
+            Assert.IsTrue(child.IsRegistered<OtherService>());
+        }
+
+        [TestMethod]
         public void unitycontainer_unity_201()
         {
             var container = GetContainer();
