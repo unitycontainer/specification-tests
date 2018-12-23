70d07b44bae21e96a42408d8a89e5d2e062cd1f8
Eugene Sadovoi <evgeni@eniks.com>
2018-12-22 20:42:39 -0500

    Allocations optimization

--- src/Registrations/IsRegistered/Setup.cs
+++ src/Registrations/IsRegistered/Setup.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
 
 namespace Unity.Specification.Registrations.IsRegistered
 {
     public abstract partial class SpecificationTests : TestFixtureBase
     {
+        private string other = "other";
+
         [TestInitialize]
         public override void Setup()
         {
             Container = GetContainer();
+
+
+            Container.RegisterType<ILogger, MockLogger>();
+            Container.RegisterType<ILogger, MockLogger>(Name);
+
+            var service = new Service();
+            Container.RegisterInstance<IService>(service);
+            Container.RegisterInstance<IService>(Name, service);
+
+            Container.RegisterType(typeof(IFoo<>), typeof(Foo<>));
+            Container.RegisterType(typeof(IFoo<>), typeof(Foo<>), Name);
         }
     }
 
@@ -24,6 +39,51 @@ namespace Unity.Specification.Registrations.IsRegistered
 
     public class SpecialLogger : ILogger
     {
+    }
+
+    public interface IService
+    {
+    }
+
+    public interface IGenericService<T>
+    {
+    }
+
+    public class Service : IService, IDisposable
+    {
+        public string ID { get; } = Guid.NewGuid().ToString();
+
+        public static int Instances = 0;
+
+        public Service()
+        {
+            Interlocked.Increment(ref Instances);
+        }
+
+        public bool Disposed = false;
+        public void Dispose()
+        {
+            Disposed = true;
+        }
+    }
+
+    public interface IFoo<TEntity>
+    {
+        TEntity Value { get; }
+    }
+
+    public class Foo<TEntity> : IFoo<TEntity>
+    {
+        public Foo()
+        {
+        }
+
+        public Foo(TEntity value)
+        {
+            Value = value;
+        }
+
+        public TEntity Value { get; }
     }
 
     #endregion
