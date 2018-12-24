8e0de61011ef97e83a6d9bf7a8efd7afa524c334
Eugene Sadovoi <evgeni@eniks.com>
2018-12-23 23:35:25 -0500

    Adding more tests

---
+++ src/Resolution/Field/Field.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unity.Specification.Resolution.Field
+{
+    public abstract partial class SpecificationTests : TestFixtureBase
+    {
+        [TestMethod]
+        public void Field()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
