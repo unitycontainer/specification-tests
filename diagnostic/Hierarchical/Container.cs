7cf8d09590e42576f629bc132620b800728fdd20
ENikS <evgeni@eniks.com>
2019-01-18 22:46:22 -0500

    Refactoring

---
+++ diagnostic/Hierarchical/Container.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unity.Specification.Diagnostic.Hierarchical
+{
+    public abstract partial class SpecificationTests
+    {
+        [TestMethod]
+        public void ChildContainer()
+        {
+            // Act
+            var child = Container.CreateChildContainer();
+
+            // Validate
+            Assert.IsNotNull(child);
+        }
+    }
+}
---
+++ diagnostic/Hierarchical/Setup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unity.Specification.Diagnostic.Hierarchical
+{
+    public abstract partial class SpecificationTests : TestFixtureBase
+    {
+        [TestInitialize]
+        public override void Setup()
+        {
+            base.Setup();
+        }
+
+        #region Test Data
+
+        #endregion
+
+    }
+}
