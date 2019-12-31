caa324389606e5b53d6b6af05a04c46932f3871f
Eugene Sadocoi <evgeni@eniks.com>
2019-12-31 14:21:06 -0800

    Adding more tests

---
+++ diagnostic/Field/Overrides.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unity.Specification.Field.Overrides.Validation
+{
+    public abstract partial class SpecificationTests : Unity.Specification.Field.Overrides.SpecificationTests
+    {
+        [TestInitialize]
+        public override void Setup() => base.Setup();
+
+    }
+}
---
+++ diagnostic/Property/Injection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unity.Specification.Property.Injection.Validation
+{
+    public abstract partial class SpecificationTests : Unity.Specification.Property.Injection.SpecificationTests
+    {
+        [TestInitialize]
+        public override void Setup() => base.Setup();
+
+    }
+}
