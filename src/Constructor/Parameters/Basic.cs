adc865d31b9fc754443fd7be45526b173f85a372
Eugene Sadocoi <evgeni@eniks.com>
2020-01-09 09:34:10 -0800

    Adding more tests

--- src/Constructor/Parameters/Basic.cs
+++ src/Constructor/Parameters/Basic.cs
@@ -13,5 +13,31 @@ namespace Unity.Specification.Constructor.Parameters
             // Validate
             Assert.IsNotNull(instance);
         }
+
+        [TestMethod]
+        public void HierarchicalParameterSelection()
+        {
+            // Arrange
+            var parent = GetContainer();
+            var child1 = parent.CreateChildContainer();
+            var child2 = child1.CreateChildContainer();
+
+            child1.RegisterType<Service>(TypeLifetime.Hierarchical);
+            child1.RegisterFactory<DependencyType>((c, t, n) => DependencyType.Create());
+            child2.RegisterInstance(Unresolvable.Create());
+
+            // Act
+            var child2Service = child2.Resolve<Service>();
+            var child1Service = child1.Resolve<Service>();
+
+            // Validate
+            Assert.IsNotNull(child2Service);
+            Assert.IsNotNull(child1Service);
+
+            Assert.AreNotSame(child2Service, child1Service);
+
+            Assert.AreEqual(1, child2Service.Parameters);
+            Assert.AreEqual(1, child1Service.Parameters);
+        }
     }
 }
--- src/Constructor/Selection/Selection.cs
+++ src/Constructor/Selection/Selection.cs
@@ -5,39 +5,59 @@ namespace Unity.Specification.Constructor.Selection
     public abstract partial class SpecificationTests
     {
         [TestMethod]
-        public void ParameterSelection()
+        public void CtorSelectionUnregistered()
         {
             // Act
-            var instance = Container.Resolve<TypeWithDynamicParameter>();
+            var result0 = iUnity0.Resolve<MultiLevelType>();
+            var result1 = iUnity1.Resolve<MultiLevelType>();
+            var result2 = iUnity2.Resolve<MultiLevelType>();
+            var result3 = iUnity3.Resolve<MultiLevelType>();
+            var result4 = iUnity4.Resolve<MultiLevelType>();
+            var result5 = iUnity5.Resolve<MultiLevelType>();
 
             // Validate
-            Assert.IsNotNull(instance);
+            Assert.IsNotNull(result0);
+            Assert.IsNotNull(result1);
+            Assert.IsNotNull(result2);
+            Assert.IsNotNull(result3);
+            Assert.IsNotNull(result4);
+            Assert.IsNotNull(result5);
+
+            Assert.AreEqual(0, result0.Level);
+            Assert.AreEqual(0, result1.Level);
+            Assert.AreEqual(0, result2.Level);
+            Assert.AreEqual(0, result3.Level);
+            Assert.AreEqual(0, result4.Level);
+            Assert.AreEqual(0, result5.Level);
+
+            Assert.Fail();
         }
 
         [TestMethod]
-        public void HierarchicalParameterSelection()
+        public void CtorSelectionUnregisteredReverse()
         {
-            // Arrange
-            var parent = GetContainer();
-            var child1 = parent.CreateChildContainer();
-            var child2 = child1.CreateChildContainer();
-
-            child1.RegisterType<Service>(TypeLifetime.Hierarchical);
-            child1.RegisterFactory<DependencyType>((c, t, n) => DependencyType.Create());
-            child2.RegisterInstance(Unresolvable.Create());
-            
             // Act
-            var child2Service = child2.Resolve<Service>();
-            var child1Service = child1.Resolve<Service>();
+            var result5 = iUnity5.Resolve<MultiLevelType>();
+            var result4 = iUnity4.Resolve<MultiLevelType>();
+            var result3 = iUnity3.Resolve<MultiLevelType>();
+            var result2 = iUnity2.Resolve<MultiLevelType>();
+            var result1 = iUnity1.Resolve<MultiLevelType>();
+            var result0 = iUnity0.Resolve<MultiLevelType>();
 
             // Validate
-            Assert.IsNotNull(child2Service);
-            Assert.IsNotNull(child1Service);
+            Assert.IsNotNull(result0);
+            Assert.IsNotNull(result1);
+            Assert.IsNotNull(result2);
+            Assert.IsNotNull(result3);
+            Assert.IsNotNull(result4);
+            Assert.IsNotNull(result5);
 
-            Assert.AreNotSame(child2Service, child1Service);
-
-            Assert.AreEqual(1, child2Service.Parameters);
-            Assert.AreEqual(1, child1Service.Parameters);
+            Assert.AreEqual(0, result0.Level);
+            Assert.AreEqual(0, result1.Level);
+            Assert.AreEqual(0, result2.Level);
+            Assert.AreEqual(0, result3.Level);
+            Assert.AreEqual(0, result4.Level);
+            Assert.AreEqual(0, result5.Level);
         }
     }
 }
