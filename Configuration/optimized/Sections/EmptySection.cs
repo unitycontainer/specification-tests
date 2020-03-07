5eb2816925fc711a3b880730e9c0e2a87acf8840
Eugene Sadocoi <evgeni@eniks.com>
2020-03-06 22:35:31 -0800

    Adding more Configuration tests

---
+++ Configuration/optimized/Sections/EmptySection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Unity.Specification.Configuration.Sections
+{
+    [TestClass]
+    public abstract partial class SpecificationTests
+    {
+
+        [DataTestMethod, DynamicData(nameof(ConfigurationFixture.Variants), typeof(ConfigurationFixture), DynamicDataSourceType.Method)]
+        public void ValidateCreatedXDoc(object[] variants)
+        {
+        }
+
+
+        [DataTestMethod, DynamicData(nameof(ConfigurationFixture.Variants), typeof(ConfigurationFixture), DynamicDataSourceType.Method)]
+        public void ValidateLoadContent(object[] variants)
+        {
+        }
+
+    }
+}
