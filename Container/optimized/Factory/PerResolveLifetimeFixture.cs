d5aaa08cd6e99f4f1abbcf47b7308e21eea3630b
ENikS <evgeni@eniks.com>
2023-03-08 16:42:21 -0800

    build update

---
+++ Container/optimized/Factory/PerResolveLifetimeFixture.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unity.Specification.Lifetime
+{
+    public abstract partial class SpecificationTests
+    {
+        [TestMethod]
+        public void PerResolveLifetimeIsHonoredWhenUsingFactory()
+        {
+            Container.RegisterFactory<SomeService>(c => new SomeService(), FactoryLifetime.PerResolve);
+
+            var rootService = Container.Resolve<AService>();
+            Assert.AreSame(rootService.SomeService, rootService.OtherService.SomeService);
+        }
+    }
+}
